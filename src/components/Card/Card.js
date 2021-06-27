3e83cf88f61b1896f5a86175576b54e0ca542efb
Debora Zandonai <debohzandonaii@gmail.com>
2021-06-27 00:26:02 -0300

    Added validation for non-existent capital

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,6 +6,27 @@ export default function Card({ country }) {
   const name = country.nameTranslations[0].value;
   const { flag, capital, _id } = country;
 
+  if (capital === "") {
+    return (
+      <>
+        <Container data-testid="card">
+          <Link data-testid="link-details" to={`/softplan/${_id}`}>
+            <div>
+              <Flag src={flag.svgFile} alt={` Bandeira ${name} `} />
+            </div>
+            <div>
+              <Country> {name}</Country>
+              <Capital>
+                {" "}
+                <strong>Capital:</strong> Não existe
+              </Capital>
+            </div>
+          </Link>
+        </Container>
+      </>
+    );
+  }
+
   return (
     <Container data-testid="card">
       <Link data-testid="link-details" to={`/softplan/${_id}`}>
